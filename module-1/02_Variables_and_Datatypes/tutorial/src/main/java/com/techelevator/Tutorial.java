e5e88bc907b99d00937dfcdb07fea39c779f5388
Christopher Luna <christtopherluna@gmail.com>
2022-09-13 09:59:18 -0400

    Missing lecture

--- module-1/02_Variables_and_Datatypes/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ module-1/02_Variables_and_Datatypes/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -19,8 +19,10 @@ public class Tutorial {
         // Step 2: Calculate the sales tax and tip
         /******************************************************************************/
 
-
-
+            final double SALES_TAX_PERCENT = 7.75;
+            double taxAmount;
+            taxAmount = SALES_TAX_PERCENT / 100 * costOfDinner;
+            double tipAmount = tipPercent / 100.0 * costOfDinner;
 
 
 
@@ -28,9 +30,9 @@ public class Tutorial {
         // Step 3: Calculate the amount per person
         /******************************************************************************/
 
+        double amountPerPerson = (costOfDinner + taxAmount + tipAmount) / numberOfGuests;
 
-
-
+        System.out.println("Amount per person: $" + amountPerPerson);
 
 
         /******************************************************************************/
@@ -38,10 +40,10 @@ public class Tutorial {
         //      guest gets, and the number left over after each guest eats their pieces.
         /******************************************************************************/
 
-
-
-
-
+        int numberOfCookies = 9;
+        int numberCookiesPerGuest = numberOfCookies / numberOfGuests;
+        int leftoverPieces = numberOfCookies % numberOfGuests;
+        System.out.println("Each guest can eat " + numberCookiesPerGuest + " cookies, with " + leftoverPieces + " left over.");
 
 
         System.out.println("********************************************");
