a1a607b6b4a37fcf5c6e5ca95d492955a90caa90
Greg Westendorf <greg@techelevator.com>
2022-09-30 11:39:04 -0400

    Module 1-15 Review Day lecture code

---
+++ module-1/15_Review_Day/lecture-in-class/src/main/java/com/techelevator/Animal.java
@@ -0,0 +1,19 @@
+package com.techelevator;
+
+public class Animal {
+    private String name;
+    private String family;
+
+    public Animal(String name, String family) {
+        this.name = name;
+        this.family = family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+}
