c47b7fddf23d448c5e23e5570978f62d46e24bed
Christopher Luna <christtopherluna@gmail.com>
2022-10-11 16:42:48 -0400

    Done

--- module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/14_oldest_parks.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/14_oldest_parks.sql
@@ -1,4 +1,8 @@
 -- 14. The name and date established of the top 10 oldest national parks.
 -- Order the results with the oldest park first.
 -- (10 rows)
-
+SELECT MIN(date_established) date_established, park_name
+FROM park
+GROUP BY park_name, date_established
+ORDER BY date_established ASC
+LIMIT 10;
