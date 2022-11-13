c7959ae6802f550faa0ef4842cfd9be9915a3f63
Greg Westendorf <greg@techelevator.com>
2022-11-13 17:19:39 -0500

    lecture update

--- module-3/06_Intro_to_JavaScript/lecture/script.js
+++ module-3/06_Intro_to_JavaScript/lecture/script.js
@@ -94,9 +94,9 @@ function Add(num1, num2) {
   return num1 + num2;
 }
 
-function Add(num1, num2, num3) {
-  return num1 + num2 + num3;
-}
+// function Add(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
 
 /*
 ########################
